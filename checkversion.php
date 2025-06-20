a3031cb162f2a2fa4a36518cfe8b6deedfa53775
Akshay Rathore <rathoreakshay373@gmail.com>
2025-06-20 22:20:48 +0530

    qwert

--- checkversion.php
+++ checkversion.php
@@ -1,3 +1,14 @@
 <?php
-echo 'PHP version: ' . phpversion();
+require 'path/to/PHPMailer.php'; // or autoload.php if using Composer
+require 'path/to/SMTP.php';
+require 'path/to/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+if (class_exists('PHPMailer\PHPMailer\PHPMailer')) {
+    echo "PHPMailer is loaded!";
+} else {
+    echo "PHPMailer is NOT loaded!";
+}
+
 ?>
